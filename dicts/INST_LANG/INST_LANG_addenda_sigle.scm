686e310a6271c1ccc4fc7f3daedfff7c4eeec1d6
ddavout <767710+ddavout@users.noreply.github.com>
2024-05-26 11:23:56 +0200

    nouvel addenda (non actif)

---
+++ dicts/INST_LANG/INST_LANG_addenda_sigle.scm
@@ -0,0 +1,16 @@
+; sigle/acronyme 
+(lex.add.entry '("ANTEN" nil(((ahn)0)((t eh n)0))))
+; OAS
+; AFP
+(lex.add.entry '("RATP" nil (((eh rh)0)((a)0 ) ((t e)0) ((p e)0))))
+(lex.add.entry '("tva" nil (((t e)0)((v e)0)((a)0))))
+(lex.add.entry '("CEDEX" nil ((("s" "e")0)(("d" "eh" "k" "s")0))))
+(lex.add.entry '("MBROLA" nil ((("eh" "m")0)(("b" "e")0)(("rh" "oh")0)((l a)0))))
+(lex.add.entry '("ANTENJ" nil (((ahn)0)((t eh n)0)((zh i)0))))
+(lex.add.entry '("adn" nil(((a)0)((d e)0)((eh n)0))))
+(lex.add.entry '("NASA" nil (((n a)0)((z a)0)))) 
+(lex.add.entry '("CAF" nil (((k a f)0))))
+(lex.add.entry '("CGT" nil (((s e)0)((zh e)0)((t e)0))))
+(lex.add.entry '("SPA_NAM" "NAM" (((eh s)0)((p e)0)((a)0))))
+(lex.add.entry '("UMR" nil ((("y")0) ((eh "m")0) ((eh "rh") 0))))
+
