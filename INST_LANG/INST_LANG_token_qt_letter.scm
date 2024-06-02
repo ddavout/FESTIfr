693f89279487856216f8018e560ff3f3257139e3
ddavout <767710+ddavout@users.noreply.github.com>
2024-06-02 08:30:33 +0200

    correction majeure de QTletter !

--- INST_LANG/INST_LANG_token_qt_letter.scm
+++ INST_LANG/INST_LANG_token_qt_letter.scm
@@ -11,33 +11,37 @@
 (define (letter token name)
 
 	(let (QT reponse lettre_seule)
-    ;(format t "\t\t\t\t\t\tici module letter sur %s\n" name)
-    (if 
-	    	(or 
-	    		(and 
-		    		(member_string fdnaw letter_list)
-		    		(item.feat token 'prepunctuation)
-		    		(item.feat token 'punc)
-		    		(or (set! lettre_seule t) t))
-	    		
-	    		(and 
-	    			(not (item.prev token))
-	    			(or (set! lettre_seule t) t))
+    (format t "\t\t\t\t\t\tici module letter sur %s\n" name)
+    (if (or 	
+    		(and (member_string fdnaw letter_list)
+			     (or 
+			    	(and 
+				    	(item.feat token 'prepunctuation)
+				    	(item.feat token 'punc)
+				    	(or (format t "ok1") t)
+				    	(or (set! lettre_seule t) t))
+			    		
+			    	(and 
+			    		(not (item.prev token))
+			    		(or (format t "ok2") t)
+			    		(or (set! lettre_seule t) t))
 
-	    		(and 
-	    			(member_string (french_downcase_string (item.feat token 'p.name)) (list "un" "une" "des" "le" "ce" "cet" "cette" "ces" "lettres" "lettre" "signe" "signes"))
-	    			(or (set! lettre_seule t) t))
+			    	(and 
+			    		(member_string (french_downcase_string (item.feat token 'p.name)) (list "un" "une" "des" "le" "ce" "cet" "cette" "ces" "lettres" "lettre" "signe" "signes"))
+			    		(or (format t "ok3") t)
+			    		(or (set! lettre_seule t) t))))
 
-		    	(and 
-					(can_be_single_letter (french_downcase_string name))
-			    	; on exclut les lettres dont le suivant porte l'apostrophe (au cas où,par ex si avant befapo)
-					(if (null? (item.next token))
-				   		t 
-				   		(not (string-equal (item.feat token 'n.whitespace) "'")))
-					; on exclut pour cause d'homophonie "M" M. Marguerite, Monsieur
-					; on ne lève pas l'ambiguité...
-					(not (string-equal (item.feat token 'punc ".") "."))
-					))
+	    	(and 
+				(can_be_single_letter (french_downcase_string name))
+		    	; on exclut les lettres dont le suivant porte l'apostrophe (au cas où,par ex si avant befapo)
+				(if (null? (item.next token))
+			   		t 
+			   		(not (string-equal (item.feat token 'n.whitespace) "'")))
+				; on exclut pour cause d'homophonie "M" M. Marguerite, Monsieur
+				; on ne lève pas l'ambiguité...
+				(not (string-equal (item.feat token 'punc ".") "."))
+				(or (format t "ok4") t)
+				))
 
 
     	(begin 
